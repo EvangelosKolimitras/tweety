b585153bb52b3c8baab670c41357f2af6986188c
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-12-30 21:09:14 +0100

    refactor: Tweet component parts are now divided into functional components for easier reusability

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,9 +1,9 @@
-import { Jumbotron, Container, Row, Col, Image } from 'react-bootstrap'
+import { Jumbotron, Container, Image } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import { TID } from '../redux/actions/authUser'
 import { ITweet, IUsers } from '../services/interface'
 import { formatDate } from '../utils/utils'
-
+import { Reply, Heart } from 'react-bootstrap-icons'
 interface DefaultRootState {
 	users: IUsers, authUser: TID
 }
@@ -12,11 +12,9 @@ interface Props {
 }
 export function Tweet(props: Props) {
 	const { users } = useSelector((state: DefaultRootState) => state.users)
-	const authUser = useSelector((state: DefaultRootState) => state.authUser)
-
+	// const authUser = useSelector((state: DefaultRootState) => state.authUser)
 	const {
 		tweet: {
-			id,
 			author,
 			text,
 			timestamp,
@@ -29,28 +27,16 @@ export function Tweet(props: Props) {
 		return Object
 			.values(users)
 			.filter(user => user.id === author)
-			.map(user => {
-				return (
-					<Container className="d-flex ">
-						<Row className="justify-content-center">
-							<Col xs={6} md={4}>
-								<Image width={100} height={100} src={user.avatarURL} roundedCircle />
-							</Col>
-							<Row className="justify-content-center">
-								<Col xs={12} md={8} lg={6}>
-									<h2> {user.name} </h2>
-									{
-										replyingTo && <h6>Replying to @{replyingTo}</h6>
-									}
-									<p> {text} </p>
-									<p> Like{likes.length > 1 && "s"}: {likes.length} </p>
-									<span>{formatDate(timestamp)}</span>
-								</Col>
-							</Row>
-						</Row>
-					</Container>
-				)
-			})
+			.map(user =>
+				<Container>
+					<Avatar avatarUrl={user.avatarURL} />
+					<Username name={user.name} />
+					<ReplyingTo replyingTo={replyingTo} />
+					<Text text={text} />
+					<Date timestamp={timestamp} />
+					<Icons likes={likes} />
+				</Container>
+			)
 	}
 
 	return (
@@ -62,3 +48,16 @@ export function Tweet(props: Props) {
 	)
 }
 
+interface AvatarProps { avatarUrl: string }
+interface UsernameProps { name: string }
+interface TextTweetProps { text: string }
+interface ReplyingToProps { replyingTo: string | null }
+interface DateProps { timestamp: number }
+interface IconsProps { likes: string[] }
+
+const Avatar = (props: AvatarProps) => <Image width={100} height={100} src={props.avatarUrl} roundedCircle />
+const Username = (props: UsernameProps) => <h2> {props.name} </h2>
+const Text = (props: TextTweetProps) => <p> {props.text} </p>
+const ReplyingTo = (props: ReplyingToProps) => (<>  { props.replyingTo && <h6>Replying to @{props.replyingTo}</h6>} </>)
+const Date = (props: DateProps) => <span>{formatDate(props.timestamp)}</span>
+const Icons = (props: IconsProps) => (<><p><Reply /></p> <p> {props.likes.length}  <Heart /> </p></>)
